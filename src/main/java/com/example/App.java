9a59b1cc84fd9a5906fcb6b7ab45cf3553524ce1
J Vandana <vandanajaagirdhar@gmail.com>
2026-06-20 22:01:34 +0530

    Initial Maven Project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to Maven and VS Code");
+        System.out.println("This is my first Maven Project!!");
+    }
+}
